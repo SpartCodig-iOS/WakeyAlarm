3b63c3807c634c1b6b666166e1ae6b59c3f7d4fd
Roy <shuwj81@daum.net>
2025-11-04 16:06:11 +0900

    [add]:  stopWatch 모듈 추가 #1

--- Projects/Presentation/Presentation/Project.swift
+++ Projects/Presentation/Presentation/Project.swift
@@ -2,7 +2,6 @@ import Foundation
 import ProjectDescription
 import DependencyPlugin
 import ProjectTemplatePlugin
-import ProjectTemplatePlugin
 import DependencyPackagePlugin
 
 let project = Project.makeAppModule(
@@ -11,8 +10,7 @@ let project = Project.makeAppModule(
   product: .staticFramework,
   settings:  .settings(),
   dependencies: [
-    .Core(implements: .Core),
-    .Shared(implements: .Shared),
+    .Presentation(implements: .StopWatch)
   ],
   sources: ["Sources/**"]
 )
