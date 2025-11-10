79a0387cb94e21809cbecdeaa2d0ac90d8495520
홍석현 <ghdtjrgus333@naver.com>
2025-11-10 10:34:16 +0900

    init: Timer 모듈 생성

---
+++ Projects/Presentation/Timer/Sources/Base.swift
@@ -0,0 +1,22 @@
+//
+//  Timer.swift
+//  Timer
+//
+//  Created by 홍석현 on 2025-11-10
+//  Copyright © 2025 DDD , Ltd., All rights reserved.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ Projects/Presentation/Timer/TimerTests/Sources/Test.swift
@@ -0,0 +1,8 @@
+//
+//  base.swift
+//  TimerTest
+//
+//  Created by 홍석현 on 2025-11-10
+//  Copyright © 2025 DDD , Ltd. All rights reserved.
+//
+
